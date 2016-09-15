0e766f894502bcd193c46de41250f7490bf4b65b
Viren Bhagat <virenb@gmail.com>
2016-09-14 22:58:43 -0400

    Updated to show number of users

--- server.js
+++ server.js
@@ -9,15 +9,35 @@ var server = http.Server(app);
 var io = socket_io(server);
 
 var count = 0;
+var clients = [];
 
-io.on('connection', function (socket) {
-	count++;
+io.on('connection', function(socket) {
+    count++;
+
+    io.sockets.on('connect', function(client) {
+    clients.push(client); 
+    console.log(clients.id);
+
+    client.on('disconnect', function() {
+        clients.splice(clients.indexOf(client), 1);
+   		 });
+    	 console.log(clients.id);
+	});
+
     console.log('Client #' + count + ' connected');
     socket.broadcast.emit('message', 'Client #' + count + ' connected');
+    console.log(`There are now ${count} user(s).`);
 
     socket.on('message', function(message) {
-        console.log('Received message:', message);
+        console.log(`Received message from user ${count}: ${message}`);
         socket.broadcast.emit('message', message);
+    });
+
+    socket.on('disconnect', function(data) {
+        console.log('User disconnected');
+        socket.broadcast.emit('User disconnected');
+        count--;
+        console.log(`There are now ${count} user(s) still here.`);
     });
 });
 
